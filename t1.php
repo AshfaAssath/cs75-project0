7cdb02d910d8affe540b32f3ec5c83d2fcb60b7f
sevody <sevody@gmail.com>
2015-05-16 11:59:30 +0000

    Initial commit

---
+++ t1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        
+    </head>
+    <body>
+        
+    </body>
+    <form>
+        <input type="text" name="dfd"/>
+        <input type="submit" value="Submit"/>
+    </form>
+    
+</html>
