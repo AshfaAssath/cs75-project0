514df5a92ba6ac67ec27cea7ee638cb04a0022e7
sevody <sevody@gmail.com>
2015-05-21 02:49:43 +0000

    try to fill the css with verson 1 in 5/21

--- html/index.php
+++ html/index.php
@@ -6,70 +6,197 @@
     <head>
         <title>Three Aces</title>
         <meta charset="utf-8">
+        <style type="text/css">
+
+            body{
+                background-color:#f5f5f5;
+            }
+            
+            div{
+                display:block;
+            }
+            h3{
+                margin:0px;
+            }
+            /*设置背景颜色*/
+            .wrapper{
+                background-color:#f5f5f5;
+                    }
+        
+            /*水平居中*/
+            .inner-wrapper{
+                width:780px;
+                margin:auto;
+                /*background:white;*/
+            }
+            
+            .categories{
+                border:1px solid #ebebeb;
+                border-bottom:none;
+                margin-top:60px;
+            }
+            
+            .header{
+                background:#f5f5f5;
+                margin:50px;
+            }
+            
+            .category{
+                background:#fafafa;
+               
+            }
+            
+            .cate-name{
+                border-bottom:1px solid #ebebeb;
+             /*   height:30px;*/
+                padding:15px;
+            }
+            
+            .items{
+                /*background:blue;*/
+            }
+            
+            .item{
+                background:white;
+                border-bottom:1px solid #ebebeb;
+                padding-left:30px;
+                height:70px;
+                position:relative;
+               
+            }
+            
+            .item-name{
+                /*margin:21px;*/
+                line-height:70px;
+                float:left;
+            }
+            
+            .item-left{
+                width:220px;
+                float:right;
+                height:70px;
+                position:relative;
+            }
+            .item-left2{
+                
+                line-height:70px;
+            }
+            .sizes{
+                /*padding:11px 16px;*/
+               /* float:left;*/
+            }
+            .size{
+                padding:1.2px;
+                font-size:10px;
+                line-height:2.5em;
+            }
+            .price{
+                position:absolute;
+                left:60px;
+                font-size:15px;
+               
+            }
+            .description{
+                
+                font-size:10px;
+                font-style:italic;
+                position:absolute;
+                left:50px;
+                bottom:10px;
+            }
+        </style>
     </head>
     
     <body>
-        <h1 style="text-align: center">Menu</h1>
-        <br />
-        <br />
         
-        <ul>
-            <?
-                $dom = simplexml_load_file("menu.xml");
-                
-                //print the categories
-                foreach ($dom->categories->category as $category)
-                {
-                    print "<li>";
-                    print $category["name"];
-                    print "<ul>";
-                    
-                    //print the items
-                    foreach ($category->items->item as $item)
-                    {
-                        print "<li>";
-                        print $item[name];
-                        
-                        //print the items with different sizes
-                        if ($item->sizes)
-                        {
-                            print "<ul>";
-                            foreach ($item->sizes->size as $size)
-                            {
-                                print "<li>";
-                                print $size[size];
-                                print "\t"."{$size->price}";
-                                print "</li>";
-                            }
-                            print "</ul>";
-                        }
-                        
-                        //print the items without different sizes
-                        else
-                            {
-                                print "<ul>";
-                                print "<li>";
-                                print "\t"."{$size->price}";
-                                print "</li>";
-                                print "</ul>";
-                            }
+        <div class="header">
+            <h1 style="text-align: center">Menu</h1>
+        </div>
+
+        <div class="wrapper">
+            <div class="inner-wrapper"> 
+                <div class="categories">
+                  
+                        <?
+                            $dom= simplexml_load_file("menu.xml");
                             
-                        if ($item->description)
-                        {
-                            print $item->description;
-                        }
-                        print "</li>";
-                        
-                        print "<br />";
-                        print "<br />";
-                    }
-                    print "</ul>";
-                    print "</li>";
-                    
-                    print "<br />";
-                    print "<br />";
-                }
-            ?>
-        </ul>
+                            //print the categories
+                            foreach ($dom->categories->category as $category)
+                            {
+                                print "<div class=\"category\">";
+                                print "<h3 class=\"cate-name\">";
+                                print $category["name"];
+                                print "</h3>";
+                                print "<div class=\"items\">";
+                  
+                                
+                                //print the items
+                                foreach ($category->items->item as $item)
+                                {
+                                    print "<div class=\"item\">";
+                                    print "<div class=\"item-name\">";
+                                    print $item[name];
+                                    print "</div>";
+                                    
+                                    print "<div class=\"item-left\">";
+                                    //print the items with different sizes
+                                    if ($item->sizes)
+                                    {
+                                        print "<div class=\"item-left1\">";
+                                        print "<div class=\"sizes\">";
+                  
+                                        foreach ($item->sizes->size as $size)
+                                        {
+                                            print "<div class=\"size\">";
+                 
+                                            print $size[size];
+                                            print "<span class=\"price\">";
+                                            print "$"."{$size->price}";
+                                            print "<span>";
+                 
+                                            print "</div>";
+                                        }
+                                        print "</div>";
+                                        print "</div>";
+                                    }
+                                    
+                                    //print the items without different sizes
+                                    else
+                                    {
+                 
+                                        print "<div class=\"item-left2\">";
+                                        print "<span class=\"price\">";
+                                        print "$"."{$size->price}";
+                 
+                                        print "</span>";
+                                        print "</div>";
+                                    }
+                                    print "</div>";  
+                                    //print the description of item if have    
+                                    if ($item->description)
+                                    { 
+                                        print "<div class=\"description\">";
+                                        print $item->description;
+                                        print "</div>";
+                                    }
+                 
+                                    print "</div>";
+                                    
+                 
+                 
+                                }
+                 
+                                print "</div>";
+                 
+                                print "</div>";
+                                
+                 
+                 
+                            }
+                        ?>
+
+                </div>
+            </div>
+        </div>
     </body>
 </html>
