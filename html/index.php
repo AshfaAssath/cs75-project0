bd91bb9706473331a89ed0c383d15bdd9a88c5c7
sevody <sevody@gmail.com>
2015-05-22 13:48:22 +0000

    fill the shopping cart's css in 5/22

--- html/index.php
+++ html/index.php
@@ -19,9 +19,21 @@
                 margin:0px;
             }
             a{
+                cursor:pointer;
                 color:white;
                 text-decoration:none;
                 
+            }
+            
+            ul{
+                margin-top:0;
+                list-style-type:none;
+                padding:0;
+                margin:0;
+            }
+            li{
+/*                display:none;*/
+                padding:10px 10px;
             }
             /*设置背景颜色*/
             .wrapper{
@@ -32,6 +44,7 @@
             .inner-wrapper{
                 width:780px;
                 margin:auto;
+                padding-right:150px;
                 /*background:white;*/
             }
             
@@ -43,7 +56,7 @@
             
             .header{
                 background:#f5f5f5;
-                margin:50px;
+                margin:70px 0 130px;
             }
             
             .category{
@@ -145,17 +158,103 @@
                 left:50px;
                 bottom:3px;
             }
+            
+            .shopping-cart{
+                background-color:#ffffff;
+                position:fixed;
+                bottom:0;
+                left:50%;
+                margin-left:315px;
+            }
+            .items-list-name{
+                color:#333333;
+                font-weight:600;
+                font-size:18px;
+            }
+            .small-font{
+                padding:3px 0px;
+                font-size:15px;
+            }
+            .items-list-size{
+                font-style:italic;
+                margin-left:105px;
+            }
+            .items-list-quantity{
+                margin-left:17px;
+                display:inline-block;
+            }
+            #substract,#add{
+                display:inline-block;
+                text-align:center;
+                width:13px;
+                height:13px;
+                line-height:13px;
+                border:1px solid #aaaaaa;
+                color:#aaaaaa;
+                cursor:pointer;
+
+            }
+            .border-line{
+                border-bottom:1px solid #bbbbbb; 
+            }
+            .top-line{
+                border-top:2px solid #fe4d3d;
+            }
+            .items-list-price{
+                color:#fe4d3d;
+                margin-left:15px;
+            }
+            .checkout-box{
+                background-color:#333333;
+                width:280px;
+                height:45px;
+            }
+            .price-count{
+                position:absolute;
+                left:32px;
+                bottom:7px;
+                font-size:20px;
+                font-size:26px;
+                color:#ffffff;
+            }
+            
+            .go-pay{
+                background-color:#8FC31F;
+                border:0;
+                border-radius:2px;
+                color:#ffffff;
+                padding:0 20px;
+                margin-top:7px;
+                margin-right:13px;
+                cursor:pointer;
+                height:31px;
+                line-height:31px;
+                text-align:center;
+                float:right;
+                font-size:16px;
+            }
         </style>
+        <script type="text/javascript" >
+            var shopCart = new function(){
+                    
+                    
+                    this.add = function(o){
+                    
+                }
+            }
+        </script>
     </head>
     
     <body>
         
         <div class="header">
-            <h1 style="text-align: center">Menu</h1>
+            <h1 style="text-align: center">Three Aces</h1>
+            
         </div>
 
         <div class="wrapper">
-            <div class="inner-wrapper"> 
+            <div class="inner-wrapper">
+            <!--    <h2 style="text-align: center">Menu</h2> -->
                 <div class="categories">
                   
                         <?
@@ -194,7 +293,8 @@
                                             print "<span class=\"price\">";
                                             print "$"."{$size->price}";
                                             print "<div class=\"icon\">";
-                                            print "<a href=\"#\" class=\"add-icon\">+</a>";
+                                            $item_detail="{id:'$item[id]',item:'$item[name]',size:'$size[size]',price:'$size->price'}";
+                                            print "<a onClick=\"shopCart.add($item_detail);\" class=\"add-icon\">+</a>";
                                             print "</div>";
                                             print "<span>";
                                             
@@ -213,7 +313,8 @@
                                         print "<span class=\"price\">";
                                         print "$"."{$size->price}";
                                         print "<div class=\"icon2\">";
-                                        print "<a href=\"#\" class=\"add-icon\">+</a>";
+                                        $item_detail="id:\'$item[id]',item:'$item[name]',price:'$size->price'}";
+                                        print "<a onClick=\"shopCart.add($item_detail)\" class=\"add-icon\">+</a>";
                                         print "</div>";
                                         print "</span>";
                         
@@ -244,6 +345,60 @@
                         ?>
 
                 </div>
+            </div>
+        </div>
+        
+        <div class="shopping-cart">
+            <div class="items-list">
+                <ul class="top-line">
+                    <li class="border-line">
+                        <div class="items-list-name">3 Piece Chicken Dinner</div>
+                        <div class="small-font">
+                            <span class="items-list-size">Sm</span>
+                            <div class="items-list-quantity">
+                                <span id="substract">-</span>
+                                <span id="number">2</span>
+                                <span id="add">+</span>
+                            </div>
+                            <span class="items-list-price">$10.00</span>
+                        </div>
+                    </li>
+                    <li class="border-line">
+                        <div class="items-list-name">Tomato & Cheese</div>
+                        <div class="small-font">
+                            <span class="items-list-size">Lg</span>
+                            <div class="items-list-quantity">
+                                <span id="substract">-</span>
+                                <span id="number">1</span>
+                                <span id="add">+</span>
+                            </div>
+                            <span class="items-list-price">7.50</span>
+                        </div>
+                    </li>
+                    <li class="border-line">
+                        <div class="items-list-name">Chicken Wing Dinner</div>
+                        <div class="small-font">
+                            <span class="items-list-size">Sm</span>
+                            <div class="items-list-quantity">
+                                <span id="substract">-</span>
+                                <span id="number">2</span>
+                                <span id="add">+</span>
+                            </div>
+                            <span class="items-list-price">$18.00</span>
+                        </div>
+                    </li>
+                    
+                    
+                </ul>
+            </div>
+            <div class="checkout-box">
+                <div class="price-count">
+                    <span id="price-count">$13.00</span>
+                </div>
+                <form method=post action="checkout.php">
+                    <input class="go-pay" type="submit" value="Checkout"/>
+                    <input id="items-list" type="hidden" name="items-list"/>
+                </form>
             </div>
         </div>
     </body>
